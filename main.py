4b7d5e4c34eba33045c9473ba94f57c86757510b
Md. A F Raian <39466280+rafan4535@users.noreply.github.com>
2022-09-23 17:36:00 +0000

    dictonary

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-def bigger_num(a, b):
-    if a > b:
-        print(a)
-    else:
-        print(b)
+# def bigger_num(a, b):
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
 
+# bigger_num(2, 1)
 
-bigger_num(2, 1)
+# # def test_bigger_num(self):
+# #     assert bigger_num(2, 1) == 2
+# #     print('ok')
 
-# def test_bigger_num(self):
-#     assert bigger_num(2, 1) == 2
-#     print('ok')
+# # test_bigger_num()
 
-# test_bigger_num()
+# fruits = ["apple", "orange"]
+# print(fruits)
+# fruits.append("grape")
+# print(fruits)
+# print(fruits[-1])
+
+# print(fruits[0:2])
+
+person = {'first_name': 'A F', 'last_name': 'Raian'}
+
+print(person['last_name'])
