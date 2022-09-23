5531e1e6a0326e2719181d45eefe3c0dfffb47fa
A. F. R <>
2022-09-23 14:04:59 +0000

    changed the variable name and try to test

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def bigger_gay(a,b):
-  print()
+def bigger_num(a, b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+
+bigger_num(2, 1)
+
+# def test_bigger_num(self):
+#     assert bigger_num(2, 1) == 2
+#     print('ok')
+
+# test_bigger_num()
